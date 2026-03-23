90f7677568c9abadd15e2b1dc658ff0dc2e28d9b
GianHerrero <gianherrero8@gmail.com>
2026-03-24 02:33:10 +0800

    Add Home component for landing page

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+// src/pages/Home.js
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NEU Library</h1>
+      <p>This is the landing page.</p>
+    </div>
+  );
+}
