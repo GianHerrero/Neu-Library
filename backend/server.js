02876d56c23fbba363fd0d66789ec300c5ae9dac
GianHerrero <gianherrero8@gmail.com>
2026-03-26 00:10:36 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (!uri) {
 }
 
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(uri)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
